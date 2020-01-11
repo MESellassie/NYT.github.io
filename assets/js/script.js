84ab313edfea5a706b95ad9b66537b5fd3670ffa
IV <irisviruet@hotmail.com>
2020-01-11 14:28:16 -0500

    console log for url works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,25 @@
-// $("button").on("click", function () {
-//     var  search= $(this).attr("");
-//     var recordNums=
-//     var startYear=
-//     var endYear=
+$( document ).ready(function() {
+$(".button1").on("click", function () {
+    
 
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=0YZMaAe5LpBkSAjwLAJQQGizh4U3cGGd";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=0YZMaAe5LpBkSAjwLAJQQGizh4U3cGGd&limit=5";
 
     $.ajax({
         url: queryURL,
         method: "GET"
         })
     .then(function(response) {
-        console.log(response)
-    });
+        console.log(response);
+        console.log(queryURL);
+
+        var  search= $(this).attr("inputAddress1");
+        console.log(search);
+        var recordNums="#Number";
+        console.log(recordNums);
+        var startYear="inputAddress2";
+        console.log(startYear);
+        var endYear="inputAddress3";
+        console.log(endYear);
+});
+});
+});
